3c7db3a5410cc2c1b2ef8aafcba8602e7893449f
anubra266 <anubra266@gmail.com>
2021-01-17 00:39:50 +0100

    Configure brand theme colors

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,25 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import "../styles/globals.css";
 
+const colors = {
+  brand: {
+    50: "#d9dae5",
+    100: "#c5c8d8",
+    200: "#9fa3be",
+    300: "#797ea4",
+    400: "#52598a",
+    500: "#394071",
+    600: "#2c3258",
+    700: "#20243f",
+    800: "#131526",
+    900: "#06070d",
+  },
+};
 function MyApp({ Component, pageProps }) {
+  const theme = extendTheme({ colors });
+
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
