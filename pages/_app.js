51820452ae308939542bfda8184a3bc008a0cbe0
anubra266 <anubra266@gmail.com>
2021-01-16 19:10:41 +0100

    Configure `ChakraProvider`

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />{" "}
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
