0b53b1a747d822dbefaa6d82b06ae8c7bb26f772
anubra266 <anubra266@gmail.com>
2021-01-17 00:40:24 +0100

    Build Site Layout

---
+++ components/layouts/site.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "~/components/navbar";
+
+const Site = ({ children }) => {
+  return (
+    <React.Fragment>
+      <Navbar />
+      {children}
+    </React.Fragment>
+  );
+};
+
+export default Site;
