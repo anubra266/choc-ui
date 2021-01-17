4ca846bcbf42fac07fab911edaead29df186a289
anubra266 <anubra266@gmail.com>
2021-01-17 03:51:04 +0100

    Fix Navbar Icon

--- components/codes/navbars/chuina/index.js
+++ components/codes/navbars/chuina/index.js
@@ -19,96 +19,96 @@ import { MobileNavButton, MobileNavContent } from "./mobile-nav";
 import SponsorButton from "./sponsor-button";
 
 const GithubIcon = (props) => (
-    <svg viewBox="0 0 20 20" {...props}>
-      <path
-        fill="currentColor"
-        d="M10 0a10 10 0 0 0-3.16 19.49c.5.1.68-.22.68-.48l-.01-1.7c-2.78.6-3.37-1.34-3.37-1.34-.46-1.16-1.11-1.47-1.11-1.47-.9-.62.07-.6.07-.6 1 .07 1.53 1.03 1.53 1.03.9 1.52 2.34 1.08 2.91.83.1-.65.35-1.09.63-1.34-2.22-.25-4.55-1.11-4.55-4.94 0-1.1.39-1.99 1.03-2.69a3.6 3.6 0 0 1 .1-2.64s.84-.27 2.75 1.02a9.58 9.58 0 0 1 5 0c1.91-1.3 2.75-1.02 2.75-1.02.55 1.37.2 2.4.1 2.64.64.7 1.03 1.6 1.03 2.69 0 3.84-2.34 4.68-4.57 4.93.36.31.68.92.68 1.85l-.01 2.75c0 .26.18.58.69.48A10 10 0 0 0 10 0"
-      />
-    </svg>
-  );
-  
-  const NavbarContent = () => {
-    const mobileNav = useDisclosure();
-  
-    const { toggleColorMode: toggleMode } = useColorMode();
-    const text = useColorModeValue("dark", "light");
-    const SwitchIcon = useColorModeValue(FaMoon, FaSun);
-    const mobileNavBtnRef = React.useRef();
-  
-    useUpdateEffect(() => {
-      mobileNavBtnRef.current?.focus();
-    }, [mobileNav.isOpen]);
-    return (
-      <React.Fragment>
-        <Flex w="100%" h="100%" px="6" align="center" justify="space-between">
-          <Flex align="center">
-            <Link href="/">
-              <HStack>
-                <Image
-                  alt="logo"
-                  src="/logo.png"
-                  style={{ height: "50px", float: "left" }}
-                  mr={2}
-                />
-                <chakra.h1
-                  mx="auto"
-                  fontSize="2em"
-                  display={{ base: "none", md: "block" }}
-                  fontFamily="heading"
-                  fontWeight="bold"
-                  style={{ float: "right" }}
-                >
-                  App
-                </chakra.h1>
-              </HStack>
-            </Link>
-          </Flex>
-  
-          <Flex
-            justify="flex-end"
-            w="100%"
-            maxW="824px"
-            align="center"
-            color="gray.400"
-          >
-            <HStack spacing="5" display={{ base: "none", md: "flex" }}>
-              <Link
-                isExternal
-                aria-label="Go to Chakra UI GitHub page"
-                href={"https://github.com/anubra266/choc-ui"}
+  <svg viewBox="0 0 20 20" {...props}>
+    <path
+      fill="currentColor"
+      d="M10 0a10 10 0 0 0-3.16 19.49c.5.1.68-.22.68-.48l-.01-1.7c-2.78.6-3.37-1.34-3.37-1.34-.46-1.16-1.11-1.47-1.11-1.47-.9-.62.07-.6.07-.6 1 .07 1.53 1.03 1.53 1.03.9 1.52 2.34 1.08 2.91.83.1-.65.35-1.09.63-1.34-2.22-.25-4.55-1.11-4.55-4.94 0-1.1.39-1.99 1.03-2.69a3.6 3.6 0 0 1 .1-2.64s.84-.27 2.75 1.02a9.58 9.58 0 0 1 5 0c1.91-1.3 2.75-1.02 2.75-1.02.55 1.37.2 2.4.1 2.64.64.7 1.03 1.6 1.03 2.69 0 3.84-2.34 4.68-4.57 4.93.36.31.68.92.68 1.85l-.01 2.75c0 .26.18.58.69.48A10 10 0 0 0 10 0"
+    />
+  </svg>
+);
+
+const NavbarContent = () => {
+  const mobileNav = useDisclosure();
+
+  const { toggleColorMode: toggleMode } = useColorMode();
+  const text = useColorModeValue("dark", "light");
+  const SwitchIcon = useColorModeValue(FaMoon, FaSun);
+  const mobileNavBtnRef = React.useRef();
+
+  useUpdateEffect(() => {
+    mobileNavBtnRef.current?.focus();
+  }, [mobileNav.isOpen]);
+  return (
+    <React.Fragment>
+      <Flex w="100%" h="100%" px="6" align="center" justify="space-between">
+        <Flex align="center">
+          <Link href="/">
+            <HStack>
+              <Image
+                alt="logo"
+                src="/logo.png"
+                style={{ height: "50px", float: "left" }}
+                mr={2}
+              />
+              <chakra.h1
+                mx="auto"
+                fontSize="2em"
+                display={{ base: "none", md: "block" }}
+                fontFamily="heading"
+                fontWeight="bold"
+                style={{ float: "right" }}
               >
-                <Icon
-                  as={GithubIcon}
-                  display="block"
-                  transition="color 0.2s"
-                  w="5"
-                  h="5"
-                  _hover={{ color: "gray.600" }}
-                />
-              </Link>
+                App
+              </chakra.h1>
             </HStack>
-            <IconButton
-              size="md"
-              fontSize="lg"
-              aria-label={`Switch to ${text} mode`}
-              variant="ghost"
-              color="current"
-              ml={{ base: "0", md: "3" }}
-              onClick={toggleMode}
-              icon={<SwitchIcon />}
-            />
-            <SponsorButton ml="5" />
-            <MobileNavButton
-              ref={mobileNavBtnRef}
-              aria-label="Open Menu"
-              onClick={mobileNav.onOpen}
-            />
-          </Flex>
+          </Link>
         </Flex>
-        <MobileNavContent isOpen={mobileNav.isOpen} onClose={mobileNav.onClose} />
-      </React.Fragment>
-    );
-  };
+
+        <Flex
+          justify="flex-end"
+          w="100%"
+          maxW="824px"
+          align="center"
+          color="gray.400"
+        >
+          <HStack spacing="5" display={{ base: "none", md: "flex" }}>
+            <Link
+              isExternal
+              aria-label="Go to Chakra UI GitHub page"
+              href={"https://github.com/anubra266/choc-ui"}
+            >
+              <Icon
+                as={GithubIcon}
+                display="block"
+                transition="color 0.2s"
+                w="5"
+                h="5"
+                _hover={{ color: "gray.600" }}
+              />
+            </Link>
+          </HStack>
+          <IconButton
+            size="md"
+            fontSize="lg"
+            aria-label={`Switch to ${text} mode`}
+            variant="ghost"
+            color="current"
+            ml={{ base: "0", md: "3" }}
+            onClick={toggleMode}
+            icon={<SwitchIcon />}
+          />
+          <SponsorButton ml="5" />
+          <MobileNavButton
+            ref={mobileNavBtnRef}
+            aria-label="Open Menu"
+            onClick={mobileNav.onOpen}
+          />
+        </Flex>
+      </Flex>
+      <MobileNavContent isOpen={mobileNav.isOpen} onClose={mobileNav.onClose} />
+    </React.Fragment>
+  );
+};
 
 const ChakraUINavbar = (props) => {
   const bg = useColorModeValue("white", "gray.800");
@@ -127,6 +127,7 @@ const ChakraUINavbar = (props) => {
       shadow={y > height ? "sm" : undefined}
       transition="box-shadow 0.2s"
       top="0"
+      position={!props.demo && "fixed"}
       zIndex="1"
       bg={bg}
       left="0"
