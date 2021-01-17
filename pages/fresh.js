d3a23df90090a784fd3e2374b2bcfc1ab40384f6
anubra266 <anubra266@gmail.com>
2021-01-17 01:10:55 +0100

    Blank view

---
+++ pages/fresh.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PageHead from "~/components/head";
+import DocsLayout from "~/components/layouts/docs/index";
+
+const Fresh = () => {
+  return (
+    <DocsLayout>
+      <PageHead title="Fresh" />
+    </DocsLayout>
+  );
+};
+
+export default Fresh;
