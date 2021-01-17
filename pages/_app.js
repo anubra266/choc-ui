444d88bf02eb1c58a1a1d8e79961e9a0a0bf2713
anubra266 <anubra266@gmail.com>
2021-01-17 02:34:54 +0100

    Register Site progress bar in root

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import NextNprogress from "nextjs-progressbar";
 import "../styles/globals.css";
 
 const colors = {
@@ -20,6 +21,12 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <ChakraProvider theme={theme}>
+      <NextNprogress
+        color="#29D"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height="2"
+      />
       <Component {...pageProps} />
     </ChakraProvider>
   );
