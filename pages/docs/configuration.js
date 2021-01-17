0aafa297c6c6a2bc2c2277e0b23169168d9a4480
anubra266 <anubra266@gmail.com>
2021-01-17 03:31:25 +0100

    Add configuration page header

--- pages/docs/configuration.js
+++ pages/docs/configuration.js
@@ -3,11 +3,13 @@ import PageHead from "~/components/head";
 import DocsLayout from "~/components/layouts/docs/index";
 import Provider from "~/components/pages/configuration/provider";
 import CustomTheme from "~/components/pages/configuration/custom-theme";
+import { PageHeader } from "~/components/docs";
 
 const Configuration = () => {
   return (
     <DocsLayout>
       <PageHead title="Configuration" />
+      <PageHeader>Configuration</PageHeader>
       <Provider />
       <CustomTheme />
     </DocsLayout>
