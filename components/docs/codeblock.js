a52111d354e753689cde8ca5153be107ba298608
anubra266 <anubra266@gmail.com>
2021-01-17 01:50:09 +0100

    Fix codeblock theme error

--- components/docs/codeblock.js
+++ components/docs/codeblock.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { coldarkDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { coldarkDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import { Box, Button, useClipboard } from "@chakra-ui/react";
 
 export const CodeBlock = (props) => {
