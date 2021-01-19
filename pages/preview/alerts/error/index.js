5d8ff3fb4b8300df06911ff0a2860577a249df05
anubra266 <anubra266@gmail.com>
2021-01-19 23:56:29 +0100

    Add error alert

---
+++ pages/preview/alerts/error/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+import {
+  chakra,
+  Box,
+  Container,
+  Icon,
+  Flex,
+  useColorModeValue,
+} from "@chakra-ui/react";
+
+import { BsLightningFill } from "react-icons/bs";
+
+const Ma = () => {
+  return (
+    <Container
+      pos="fixed"
+      top="50%"
+      left="50%"
+      transform="translate(-50%,-50%)"
+      bg="gray.600"
+      p={50}
+      width="3xl"
+      centerContent
+    >
+      <Flex
+        maxW="sm"
+        width="100%"
+        mx="auto"
+        bg={useColorModeValue("white", "gray.800")}
+        boxShadow="md"
+        borderRadius="lg"
+        overflow="hidden"
+      >
+        <Flex justifyContent="center" alignItems="center" w={12} bg="red.500">
+          <Icon as={BsLightningFill} color="white" boxSize={6} />
+        </Flex>
+
+        <Box mx={-3} py={2} px={4}>
+          <Box mx={3}>
+            <chakra.span
+              color={useColorModeValue("red.500", "red.400")}
+              fontWeight="bold"
+            >
+              Error
+            </chakra.span>
+            <chakra.p
+              color={useColorModeValue("gray.600", "gray.200")}
+              fontSize="sm"
+            >
+              Your email is already used!
+            </chakra.p>
+          </Box>
+        </Box>
+      </Flex>
+    </Container>
+  );
+};
+
+export default Ma;
