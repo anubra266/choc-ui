bb121669a266e6c6149671eae899337e1fa52b28
anubra266 <anubra266@gmail.com>
2021-01-20 18:25:34 +0100

    Fix Homelink in iframe

--- components/site-settings.js
+++ components/site-settings.js
@@ -3,11 +3,20 @@ import { ThemeSwitcher } from "~/components/theme-switcher";
 import { Stack } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 
+function inIframe() {
+  try {
+    return window.self !== window.top;
+  } catch (e) {
+    return true;
+  }
+}
+
 export default function Ss() {
   const router = useRouter();
   const isPreview = router.pathname.includes("preview");
   return (
-    isPreview && (
+    isPreview &&
+    !inIframe() && (
       <Stack pos="fixed" right={0} bottom={5}>
         <HomeLink />
         <ThemeSwitcher />
--- components/theme-switcher.js
+++ components/theme-switcher.js
@@ -1,36 +1,22 @@
-import {
-  IconButton,
-  useColorMode,
-  useColorModeValue,
-} from "@chakra-ui/react";
+import { IconButton, useColorMode, useColorModeValue } from "@chakra-ui/react";
 import { FaMoon, FaSun } from "react-icons/fa";
-
-function inIframe() {
-  try {
-    return window.self !== window.top;
-  } catch (e) {
-    return true;
-  }
-}
 
 export const ThemeSwitcher = () => {
   const SwitchIcon = useColorModeValue(FaMoon, FaSun);
   const text = useColorModeValue("dark", "light");
   const { toggleColorMode: toggleMode } = useColorMode();
   return (
-    !inIframe() && (
-      <IconButton
-        borderRightRadius={0}
-        borderLeftRadius="full"
-        size="md"
-        fontSize="lg"
-        aria-label={`Switch to ${text} mode`}
-        variant="solid"
-        colorScheme="brand"
-        ml={{ base: "0", md: "3" }}
-        onClick={toggleMode}
-        icon={<SwitchIcon />}
-      />
-    )
+    <IconButton
+      borderRightRadius={0}
+      borderLeftRadius="full"
+      size="md"
+      fontSize="lg"
+      aria-label={`Switch to ${text} mode`}
+      variant="solid"
+      colorScheme="brand"
+      ml={{ base: "0", md: "3" }}
+      onClick={toggleMode}
+      icon={<SwitchIcon />}
+    />
   );
 };
