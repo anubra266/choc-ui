ef50af476c52a90faed340cb2a73fbb5607d6627
anubra266 <anubra266@gmail.com>
2021-01-21 21:16:28 +0100

    Fix Centre 2x2 grid Feature

--- pages/preview/features/c2g/index.js
+++ pages/preview/features/c2g/index.js
@@ -6,6 +6,7 @@ import {
   Flex,
   useColorModeValue,
   Icon,
+  Stack,
 } from "@chakra-ui/react";
 
 const Feature = (props) => {
@@ -95,12 +96,12 @@ const Ma = () => {
           </Box>
 
           <Box mt={10}>
-            <SimpleGrid
-              spacingX={{ base: 10, md: 0 }}
-              columns={{ md: 2 }}
-              rowGap={{ md: 8 }}
-              columnGap={{ md: 10 }}
-              class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10"
+            <Stack
+              spacing={{ base: 10, md: 0 }}
+              display={{ md: "grid" }}
+              gridTemplateColumns={{ md: "repeat(2,1fr)" }}
+              gridColumnGap={{ md: 8 }}
+              gridRowGap={{ md: 10 }}
             >
               <Feature
                 title="Competitive exchange rates"
@@ -157,7 +158,7 @@ const Ma = () => {
                   d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
                 />
               </Feature>
-            </SimpleGrid>
+            </Stack>
           </Box>
         </Box>
       </Box>
