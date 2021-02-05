0ef10eb0cbe325a8070252e163d942adc443b951
Michael Grigoryan <michael.grigoryan25@gmail.com>
2021-02-05 20:53:44 +0400

    Added Button with Badge

--- pages/preview/features/c2g/index.js
+++ pages/preview/features/c2g/index.js
@@ -106,9 +106,9 @@ export default function C2g() {
                 title="Competitive exchange rates"
                 icon={
                   <path
-                    stroke-linecap="round"
+                    strokeLinecap="round"
                     strokeLinejoin="round"
-                    stroke-width="2"
+                    strokeWidth="2"
                     d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
                   />
                 }
@@ -122,9 +122,9 @@ export default function C2g() {
                 title=" No hidden fees"
                 description={
                   <path
-                    stroke-linecap="round"
+                    strokeLinecap="round"
                     strokeLinejoin="round"
-                    stroke-width="2"
+                    strokeWidth="2"
                     d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
                   />
                 }
@@ -138,9 +138,9 @@ export default function C2g() {
                 title="Transfers are instant"
                 icon={
                   <path
-                    stroke-linecap="round"
+                    strokeLinecap="round"
                     strokeLinejoin="round"
-                    stroke-width="2"
+                    strokeWidth="2"
                     d="M13 10V3L4 14h7v7l9-11h-7z"
                   />
                 }
@@ -154,9 +154,9 @@ export default function C2g() {
                 title="Mobile notifications"
                 description={
                   <path
-                    stroke-linecap="round"
+                    strokeLinecap="round"
                     strokeLinejoin="round"
-                    stroke-width="2"
+                    strokeWidth="2"
                     d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
                   />
                 }
