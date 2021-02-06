d52cb285a025b2d9f1e532ca3f06ec41c59af11b
anubra266 <anubra266@gmail.com>
2021-02-06 16:22:54 +0100

    Persist brand choice on site close.

--- pages/_app.js
+++ pages/_app.js
@@ -11,8 +11,10 @@ const config = {
   useSystemColorMode: false,
 };
 function MyApp({ Component, pageProps }) {
-  const [brand, setBrand] = useState("default");
-
+  const [brand, setBrand] = useState(window.localStorage.getItem('brand')||"default");
+  React.useEffect(()=>{
+    window.localStorage.setItem('brand', brand);
+  }[brand])
   const theme = extendTheme({ colors: { brand: presets(brand) }, config });
   const themeProps = useMemo(
     () => ({
