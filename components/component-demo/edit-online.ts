f3548ed2e4626a7d58d07421d6cd5959eb2fcde5
anubra266 <anubra266@gmail.com>
2021-03-10 18:46:49 +0100

    🐛 Fix Codesandbox api; binary value

--- components/component-demo/edit-online.ts
+++ components/component-demo/edit-online.ts
@@ -5,25 +5,29 @@ import { html } from "./codesandbox/html";
 
 export const editOnlineCds = (code, path) => {
   const section = path.split("/")[0];
-  const json_config = {
+  const json_config = `${{
     ...package_json,
     name: `Choc UI ${section} Codesandbox`,
     description: `Edit Choc UI ${section} Code`,
-  };
+  }}`;
 
   const parameters = getParameters({
     files: {
       "public/index.html": {
         content: html(path),
+        isBinary: false,
       },
       "src/index.js": {
         content: index,
+        isBinary: false,
       },
       "src/App.js": {
         content: App(code),
+        isBinary: false,
       },
       "package.json": {
         content: json_config,
+        isBinary: false,
       },
     },
   });
