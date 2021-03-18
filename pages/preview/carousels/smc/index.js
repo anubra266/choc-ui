c15c2d85fa581e9e25c8ba76ac263080a976cff4
anubra266 <anubra266@gmail.com>
2021-03-19 00:20:43 +0100

    💄 Update Carousel backgrounds

--- pages/preview/carousels/smc/index.js
+++ pages/preview/carousels/smc/index.js
@@ -11,13 +11,24 @@ import {
 const Component = () => {
   const slides = [
     {
-      img: "https://www.w3schools.com/howto/img_nature_wide.jpg",
+      img:
+        "https://images.pexels.com/photos/2599537/pexels-photo-2599537.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
     },
     {
-      img: "https://www.w3schools.com/howto/img_snow_wide.jpg",
+      img:
+        "https://images.pexels.com/photos/2714581/pexels-photo-2714581.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
     },
     {
-      img: "https://www.w3schools.com/howto/img_mountains_wide.jpg",
+      img:
+        "https://images.pexels.com/photos/2878019/pexels-photo-2878019.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+    },
+    {
+      img:
+        "https://images.pexels.com/photos/5409751/pexels-photo-5409751.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+    },
+    {
+      img:
+        "https://images.pexels.com/photos/3124111/pexels-photo-3124111.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
     },
   ];
 
--- pages/preview/carousels/wcp/index.js
+++ pages/preview/carousels/wcp/index.js
@@ -33,20 +33,35 @@ const Component = () => {
 
   const slides = [
     {
-      img: "https://www.w3schools.com/howto/img_nature_wide.jpg",
+      img:
+        "https://images.pexels.com/photos/2599537/pexels-photo-2599537.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
       label: "First Slide",
       description: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
     },
     {
-      img: "https://www.w3schools.com/howto/img_snow_wide.jpg",
+      img:
+        "https://images.pexels.com/photos/2714581/pexels-photo-2714581.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
       label: "Second Slide",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     },
     {
-      img: "https://www.w3schools.com/howto/img_mountains_wide.jpg",
+      img:
+        "https://images.pexels.com/photos/2878019/pexels-photo-2878019.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
       label: "Third Slide",
       description:
         "Praesent commodo cursus magna, vel scelerisque nisl consectetur.",
+    },
+    {
+      img:
+        "https://images.pexels.com/photos/5409751/pexels-photo-5409751.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+      label: "Fourth Slide",
+      description: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+    },
+    {
+      img:
+        "https://images.pexels.com/photos/3124111/pexels-photo-3124111.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      label: "Fifth Slide",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     },
   ];
 
