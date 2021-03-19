276bb7a0875c4e276369c32126231bd65d089720
anubra266 <anubra266@gmail.com>
2021-03-19 13:04:49 +0100

    ✨ Transition theme change

--- theme/styles.ts
+++ theme/styles.ts
@@ -6,6 +6,7 @@ const styles = {
       fontFamily: "body",
       color: mode("gray.800", "whiteAlpha.900")(props),
       bg: mode("white", "gray.800")(props),
+      transition: "background .4s ease-in-out",
     },
     html: {
       minH: "100%",
