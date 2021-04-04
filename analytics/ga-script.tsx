cec6502b2208b76d62ebb4fdcc9bdc4fe65b287f
anubra266 <anubra266@gmail.com>
2021-04-04 21:04:17 +0100

    📈 Add adsense script

--- analytics/ga-script.tsx
+++ analytics/ga-script.tsx
@@ -5,6 +5,11 @@ const GA_TRACKING_ID = "G-YVRM2PTZFG";
 const GAScript = () => (
   <>
     <script
+      data-ad-client="ca-pub-9858691647622096"
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+    ></script>
+    <script
       async
       src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
     />
