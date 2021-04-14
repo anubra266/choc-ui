989baca708a8052976271b059781645eab4ab369
anubra266 <anubra266@gmail.com>
2021-04-14 13:15:02 +0100

    📈 Remove oribi

--- analytics/ga-script.tsx
+++ analytics/ga-script.tsx
@@ -5,12 +5,6 @@ const GA_TRACKING_ID = "G-YVRM2PTZFG";
 const GAScript = () => (
   <>
     <script
-      type="application/javascript"
-      dangerouslySetInnerHTML={{
-        __html: ` (function(b,o,n,g,s,r,c){if(b[s])return;b[s]={};b[s].scriptToken="Xy0zNTUwNjI1NzU";b[s].callsQueue=[];b[s].api=function(){b[s].callsQueue.push(arguments);};r=o.createElement(n);c=o.getElementsByTagName(n)[0];r.async=1;r.src=g;r.id=s+n;c.parentNode.insertBefore(r,c);})(window,document,"script","https://cdn.oribi.io/Xy0zNTUwNjI1NzU/oribi.js","ORIBI");`,
-      }}
-    ></script>
-    <script
       async
       src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
     />
