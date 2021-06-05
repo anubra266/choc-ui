9d71cb1fee3d9130c435ebda1aa5ec4f3e162a4b
anubra266 <anubra266@gmail.com>
2021-06-06 00:58:43 +0100

    💄 Add ScrollTop  Button

---
+++ components/to-top.tsx
@@ -0,0 +1,29 @@
+import { IconButton } from "@chakra-ui/button";
+import { useBoolean } from "@chakra-ui/hooks";
+import React, { useState } from "react";
+import { FiArrowUp } from "react-icons/fi";
+
+const ToTop = () => {
+  const scrollToTop = () => window.scroll({ top: 0, behavior: "smooth" });
+  const getVisible = () =>
+    document.body.scrollTop > 20 || document.documentElement.scrollTop > 20;
+  const [isVisible, setIsVisible] = useState(() => getVisible());
+  window.onscroll = () => setIsVisible(() => getVisible());
+
+  return (
+    <IconButton
+      aria-label="Back to top"
+      title="Back to top"
+      icon={<FiArrowUp />}
+      rounded="full"
+      pos="fixed"
+      bottom="30"
+      right="4"
+      colorScheme="brand"
+      onClick={scrollToTop}
+      visibility={isVisible ? "visible" : "hidden"}
+    />
+  );
+};
+
+export default ToTop;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import SiteSettings from "components/site-settings";
 import PageHead from "components/head/static";
 
 import splitbee from "@splitbee/web";
+import ToTop from "components/to-top";
 
 splitbee.init();
 
@@ -13,6 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <PageHead />
       <Component {...pageProps} />
       <SiteSettings />
+      <ToTop />
     </Theme>
   );
 }
