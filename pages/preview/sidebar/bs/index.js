59c8e997cd01e47d3181c3767a6953e8319feb2c
anubra266 <anubra266@gmail.com>
2021-06-09 16:22:43 +0100

    ✨ add brands sidebar

--- pages/preview/sidebar/bs/index.js
+++ pages/preview/sidebar/bs/index.js
@@ -1,7 +1,6 @@
 import {
   Avatar,
   Box,
-  Collapse,
   Drawer,
   DrawerContent,
   DrawerOverlay,
@@ -20,13 +19,12 @@ import { AiFillGift } from "react-icons/ai";
 import { BsGearFill } from "react-icons/bs";
 import { FiMenu, FiSearch } from "react-icons/fi";
 import { HiCode, HiCollection } from "react-icons/hi";
-import { MdHome, MdKeyboardArrowRight } from "react-icons/md";
+import { MdHome } from "react-icons/md";
 import React from "react";
 import { Logo } from "@choc-ui/logo";
 
 export default function Swibc() {
   const sidebar = useDisclosure();
-  const integrations = useDisclosure();
 
   const NavItem = (props) => {
     const { icon, children, ...rest } = props;
@@ -34,13 +32,14 @@ export default function Swibc() {
       <Flex
         align="center"
         px="4"
-        pl="4"
+        mx="2"
+        rounded="md"
         py="3"
         cursor="pointer"
-        color={useColorModeValue("inherit", "gray.400")}
+        color="whiteAlpha.700"
         _hover={{
-          bg: useColorModeValue("gray.100", "gray.900"),
-          color: useColorModeValue("gray.900", "gray.200"),
+          bg: "blackAlpha.300",
+          color: "whiteAlpha.900",
         }}
         role="group"
         fontWeight="semibold"
@@ -52,7 +51,7 @@ export default function Swibc() {
             mr="2"
             boxSize="4"
             _groupHover={{
-              color: useColorModeValue("gray.600", "gray.300"),
+              color: "gray.300",
             }}
             as={icon}
           />
@@ -73,20 +72,15 @@ export default function Swibc() {
       pb="10"
       overflowX="hidden"
       overflowY="auto"
-      bg={useColorModeValue("white", "gray.800")}
-      borderColor={useColorModeValue("inherit", "gray.700")}
+      bg="brand.600"
+      borderColor="blackAlpha.300"
       borderRightWidth="1px"
       w="60"
       {...props}
     >
       <Flex px="4" py="5" align="center">
         <Logo />
-        <Text
-          fontSize="2xl"
-          ml="2"
-          color={useColorModeValue("brand.500", "white")}
-          fontWeight="semibold"
-        >
+        <Text fontSize="2xl" ml="2" color="white" fontWeight="semibold">
           Choc UI
         </Text>
       </Flex>
@@ -101,25 +95,7 @@ export default function Swibc() {
         <NavItem icon={FaRss}>Articles</NavItem>
         <NavItem icon={HiCollection}>Collections</NavItem>
         <NavItem icon={FaClipboardCheck}>Checklists</NavItem>
-        <NavItem icon={HiCode} onClick={integrations.onToggle}>
-          Integrations
-          <Icon
-            as={MdKeyboardArrowRight}
-            ml="auto"
-            transform={integrations.isOpen && "rotate(90deg)"}
-          />
-        </NavItem>
-        <Collapse in={integrations.isOpen}>
-          <NavItem pl="12" py="2">
-            Shopify
-          </NavItem>
-          <NavItem pl="12" py="2">
-            Slack
-          </NavItem>
-          <NavItem pl="12" py="2">
-            Zapier
-          </NavItem>
-        </Collapse>
+        <NavItem icon={HiCode}>Integrations</NavItem>
         <NavItem icon={AiFillGift}>Changelog</NavItem>
         <NavItem icon={BsGearFill}>Settings</NavItem>
       </Flex>
@@ -151,7 +127,7 @@ export default function Swibc() {
           px="4"
           bg={useColorModeValue("white", "gray.800")}
           borderBottomWidth="1px"
-          borderColor={useColorModeValue("inherit", "gray.700")}
+          borderColor="blackAlpha.300"
           h="14"
         >
           <IconButton
