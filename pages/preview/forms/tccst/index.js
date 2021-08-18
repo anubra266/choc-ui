d89d71d3eacb0a385715f3ee3fe93397e28a846d
Ismail D <didouhismael@gmail.com>
2021-08-18 22:00:32 +0200

    Fix SVG attribute typo
    Renamed the attribute `strokLinecap ` into `strokeLinecap`

--- pages/preview/forms/tccst/index.js
+++ pages/preview/forms/tccst/index.js
@@ -182,7 +182,7 @@ export default function Component() {
                         <path
                           d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
                           strokeWidth="2"
-                          strokLinecap="round"
+                          strokeLinecap="round"
                           strokeLinejoin="round"
                         />
                       </Icon>
